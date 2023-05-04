077441d54dd4bde88704c59fe6fd594c4bcf793c
ombasaMukhwami <mukhwami@gmail.com>
2023-05-04 11:04:07 +0300

    initial files

---
+++ src/main/java/com/ombasa/Main.java
@@ -0,0 +1,11 @@
+package com.ombasa;
+
+public final class Main {
+
+    private Main() {
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
